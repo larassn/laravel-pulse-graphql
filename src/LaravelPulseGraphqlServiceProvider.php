0d5cf1b32e7ebd568d1e35b357e39b6e57e91f08
Ali Sasani <a_S47_1@yahoo.com>
2024-11-05 23:42:00 +0330

    [feat] addpend middleware to execution_middleware

--- src/LaravelPulseGraphqlServiceProvider.php
+++ src/LaravelPulseGraphqlServiceProvider.php
@@ -4,6 +4,7 @@ namespace LaraSsn\LaravelPulseGraphql;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use LaraSsn\LaravelPulseGraphql\Http\Middlewares\SetStartedAtGraphGLMiddleware;
 use LaraSsn\LaravelPulseGraphql\Livewire\GraphqlRequest;
 use Livewire\LivewireManager;
 
@@ -16,5 +17,10 @@ class LaravelPulseGraphqlServiceProvider extends ServiceProvider
         $this->callAfterResolving('livewire', function (LivewireManager $livewire, Application $app) {
             $livewire->component('pulse.graphql', GraphqlRequest::class);
         });
+
+        // append middleware to execution_middleware
+        config([
+            'graphql.execution_middleware' => [SetStartedAtGraphGLMiddleware::class, ...config('graphql.execution_middleware', [])],
+        ]);
     }
 }
